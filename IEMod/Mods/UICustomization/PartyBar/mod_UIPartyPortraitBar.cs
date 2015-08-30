9d31a0fb0301c382eba74a63cdce55d592dd32c8
tjayharvey <tjayharvey@gmail.com>
2015-08-30 10:11:12 -0700

    Better commenting in portrait bar

--- IEMod/Mods/UICustomization/PartyBar/mod_UIPartyPortraitBar.cs
+++ IEMod/Mods/UICustomization/PartyBar/mod_UIPartyPortraitBar.cs
@@ -18,15 +18,12 @@ namespace IEMod.Mods.PartyBar {
             //    mPortraits.localPosition = new Vector3(desiredXPosition, single, partyPortrait.z);
             //}
 
-            //TODO: GR 29/8 - manually correct this section to be consistent with 2.0 using an IDE
-            /*StringBuilder sb = new StringBuilder();
-        sb.Append("P! RepositionPortraitsNew() start. PartyBarToggle = " + PlayerPrefs.GetInt("PartyBarToggled", 0).ToString() + "\n");*/
+            //REWRITE
             for (int i = 0; i < this.m_Portraits.Length; i++) {
 				this.m_Portraits[i].transform.localPosition = mod_UIPartyPortrait.IsVertical
 					? new Vector3(0f, (PortraitWidth + 20) * (i * -1), this.PartyPortrait.transform.localPosition.z)
 					: new Vector3(this.GetDesiredXPosition(i), 0f, this.PartyPortrait.transform.localPosition.z);
-			}
-			//Debug.Log(sb.ToString());
+			}			
 		}
 	}
 
