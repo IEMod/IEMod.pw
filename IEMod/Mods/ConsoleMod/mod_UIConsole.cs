b2b6b38153122537549069c6b69193d3b9300b3e
tjayharvey <tjayharvey@gmail.com>
2015-08-30 10:08:39 -0700

    Small Cleanup Fixes

--- IEMod/Mods/ConsoleMod/mod_UIConsole.cs
+++ IEMod/Mods/ConsoleMod/mod_UIConsole.cs
@@ -3,13 +3,12 @@ using UnityEngine;
 
 namespace IEMod.Mods.ConsoleMod {
 	[ModifiesType()]
-	public  class mod_UIConsole : UIConsole
+	public class mod_UIConsole : UIConsole
 	{
-		
-
-		[ModifiesMember("AdjustBackground")]
-		private void AdjustBackgroundNew(Vector2 delta)
+        [ModifiesMember("AdjustBackground")]
+		private void mod_AdjustBackground(Vector2 delta)
 		{
+            //ORIGINAL CODE
 			this.m_SizeChanged = true;
 			float num = this.MatchScale.GetScaleX() / InGameUILayout.Root.pixelSizeAdjustment;
 			float num2 = this.MatchScale.GetScaleY() / InGameUILayout.Root.pixelSizeAdjustment;
@@ -29,7 +28,8 @@ namespace IEMod.Mods.ConsoleMod {
 			{
 				delta.y /= num2;
 			}
-			//TODO: GR 29/9 - manually check if this block is compatible with 2.0 block
+
+			//FOLLOWING BLOCK IS A REWRITE OF A SECTION OF THE ORIGINAL. PURPOSE OF REWRITE UNKNOWN
 			Transform transform = this.CornerHandle.gameObject.transform;
 			transform.localPosition += new Vector3(delta.x, delta.y, 0f);  //GameUtilities.V2ToV3(delta); <-- using V2ToV3 doesn't work, for some reason.. caused a bug
 			//float vecX = (this.ScreenBR.transform.localPosition.x - (this.LeftMarkerPosition.x + 28f)) - this.MaxMargins.x; // original code but it wasn't exactly written like that
@@ -43,6 +43,7 @@ namespace IEMod.Mods.ConsoleMod {
 			this.Background.transform.localScale = new Vector3(num3, num4, this.Background.transform.localScale.z);
 			if (this.Background.transform.localScale.y > ((vector.y - this.MinSize.y) / 2f))
 			{
+                //RESUME ORIGINAL CODE TO END OF FUNCTION
 				this.TransitionAnchorPoint.pixelOffset = new Vector2(-42f, -42f);
 			}
 			else
