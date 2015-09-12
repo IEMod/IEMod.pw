281f5ee6a3d63a21c5881b841c0315be84872415
tariel36 <tariel36@tlen.pl>
2015-09-12 14:33:58 +0200

    Added maximum count of adventurers hired in tavern mod.

---
+++ IEMod/Mods/MaxAdventurers/mod_PartyMemberAI.cs
@@ -0,0 +1,38 @@
+﻿using IEMod.Mods.Options;
+using Patchwork.Attributes;
+using System.Reflection;
+
+namespace IEMod.Mods.MaxAdventurers
+{
+    [ModifiesType]
+    public class mod_PartyMemberAI : PartyMemberAI
+    {
+        [ToggleFieldAttributes(FieldAttributes.InitOnly)]
+        [ModifiesMember(nameof(MaxAdventurers))]        
+        public static int mod_MaxAdventurers;
+
+        [NewMember()]
+        public static void newUpdateMaxAdventurers()
+        {
+            switch (IEModOptions.MaxAdventurersCount)
+            {
+                case IEModOptions.MaxAdventurersOptions.Double_16:
+                    MaxAdventurers = 16;
+                    break;
+                case IEModOptions.MaxAdventurersOptions.Triple_24:
+                    MaxAdventurers = 24;
+                    break;
+                case IEModOptions.MaxAdventurersOptions.Quadra_32:
+                    MaxAdventurers = 32;
+                    break;
+                case IEModOptions.MaxAdventurersOptions.OneHundredTwentyEight:
+                    MaxAdventurers = 128;
+                    break;
+                case IEModOptions.MaxAdventurersOptions.Normal_8:
+                default:
+                    MaxAdventurers = 8;
+                    break;
+            }
+        }
+    }
+}
