73fbc29077f38d89d4dd5f5a03ce2c0a79d86d80
GregRos <life.gregr@gmail.com>
2015-09-14 00:20:35 +0300

    Some more work

--- IEMod/Mods/Targeting/mod_AttackBase.cs
+++ IEMod/Mods/Targeting/mod_AttackBase.cs
@@ -1,16 +1,41 @@
-﻿using IEMod.Mods.Options;
+﻿using IEMod.Helpers;
+using IEMod.Mods.Options;
 using Patchwork.Attributes;
 using UnityEngine;
 
 namespace IEMod.Mods.Targeting {
 	[ModifiesType]
 	internal class mod_AttackBase : AttackBase {
+		[NewMember]
+		private bool HostileEvenIfConfused(GameObject target, GameObject caster) {
+			var targetFaction = target.GetComponent<Faction>();
+			var casterFaction = caster.GetComponent<Faction>();
+			var targetAiController = GameUtilities.FindActiveAIController(target);
+			if (targetFaction?.IsHostile(caster) == true || casterFaction?.IsHostile(target) == true) {
+				//they're actually hostile
+				return true;
+			}
+			if (targetAiController == null) {
+				//not AI controller. Can never be confused.
+				return false;
+			}
+			if (!IEModOptions.TargetTurnedEnemies) {
+				//no more checks if the option is disabled.
+				return false;
+			}
+			if (casterFaction?.CurrentTeam == null) {
+				//dunno if this is even necessary... sigh...
+				return false;
+			}
+			var targetOriginal = targetAiController.GetOriginalTeam()?.GetRelationship(casterFaction.CurrentTeam)
+				== Faction.Relationship.Hostile;
+
+			return targetOriginal;
+		}
+
 		[ModifiesMember("IsValidTarget")]
-		public bool IsValidTargetNew(GameObject target, GameObject caster, TargetType validType) {
-			var confusedArentFriends = IEModOptions.TargetTurnedEnemies;
-
-			//(original decompiled code:)
-
+		public bool mod_IsValidTarget(GameObject target, GameObject caster, TargetType validType) {
+			//+ UNTOUCHED ORIGINAL CODE
 			if (IgnoreValidTargetCheck()) {
 				return true;
 			}
@@ -20,149 +45,162 @@ namespace IEMod.Mods.Targeting {
 			if (caster == null) {
 				caster = Owner;
 			}
-			var component1 = target.GetComponent<Health>();
-			if (component1 == null || !component1.CanBeTargeted || !target.activeInHierarchy) {
+			Health component = target.GetComponent<Health>();
+			if (component == null || !component.CanBeTargeted || !target.activeInHierarchy) {
 				return false;
 			}
-			var activeAiController = GameUtilities.FindActiveAIController(target);
-			if (activeAiController != null && activeAiController.IsBusy) {
+			if (component.Dead) {
+				if (!TargetTypeUtils.ValidTargetDead(validType)) {
+					return false;
+				}
+			} else if (component.Unconscious && !TargetTypeUtils.ValidTargetUnconscious(validType)) {
 				return false;
 			}
-			var component2 = target.GetComponent<CharacterStats>();
-			if (ApplyOnceOnly && m_ability != null
-				&& (component2 != null && component2.CountStatusEffects(m_ability) > 0)) {
+			AIController aIController = GameUtilities.FindActiveAIController(target);
+			if (aIController != null && aIController.IsBusy) {
 				return false;
 			}
-			var component3 = caster.GetComponent<Faction>();
-			var targetFaction = target.GetComponent<Faction>();
+			CharacterStats characterStat = target.GetComponent<CharacterStats>();
+			if (ApplyOnceOnly && m_ability != null && characterStat != null
+				&& characterStat.CountStatusEffects(m_ability) > 0) {
+				return false;
+			}
+			Faction casterFaction = caster.GetComponent<Faction>();
+			Faction targetFaction = target.GetComponent<Faction>();
+			//+ END ORIGINAL CODE
+			//!+ MODIFICATIONS
+			//Replaced all appearances of 'faction.IsHostile(target) || component1 != null && component1.IsHostile(caster)' with 'HostileEvenIfConfused(target,caster)'
 			switch (validType) {
-				case TargetType.All:
-					if (!component1.Unconscious && !component1.Dead) {
+				case TargetType.All: {
+					//!+ADDED CODE
+					if (IEModOptions.DisableFriendlyFire) {
+						return casterFaction?.isPartyMember != true || targetFaction?.isPartyMember != true || HostileEvenIfConfused(target, caster);
+					}
+					//!+ END ADD
+					return true;
+				}
+				case TargetType.Hostile: {
+					if (HostileEvenIfConfused(target, caster)) {
 						return true;
 					}
 					break;
-				case TargetType.Hostile:
-					//(this part is modified)
-					if (component1.Unconscious || component1.Dead) { break; }
-					if ((component3 != null && component3.IsHostile(target))
-						|| (targetFaction != null && targetFaction.IsHostile(caster))) {
-						return true;
-					}
-
-					if (confusedArentFriends && activeAiController != null && component3 != null
-						&& activeAiController.GetOriginalTeam().GetRelationship(component3.CurrentTeam) == Faction.Relationship.Hostile) {
+				}
+				case TargetType.Friendly: {
+					if (!HostileEvenIfConfused(target, caster)) {
 						return true;
 					}
 					break;
-				case TargetType.Friendly:
-					if (component1.Unconscious || component1.Dead) { break; }
-					if (component3 != null && !component3.IsHostile(target)
-						&& (targetFaction == null || !targetFaction.IsHostile(caster))) {
-						if (!confusedArentFriends || activeAiController == null
-							|| activeAiController.GetOriginalTeam().GetRelationship(component3.CurrentTeam) != Faction.Relationship.Hostile) {
-							return true;
-						}
-					}
-					break;
-				case TargetType.FriendlyUnconscious:
-					if (component3 != null && component1.Unconscious && !component3.IsHostile(target)
+				}
+				case TargetType.FriendlyUnconscious: {
+					if (casterFaction != null && component.Unconscious && !component.Dead && !casterFaction.IsHostile(target)
 						&& (targetFaction == null || !targetFaction.IsHostile(caster))) {
 						return true;
 					}
 					break;
-				case TargetType.AllDeadOrUnconscious:
-					if (component1.Unconscious || component1.Dead) {
+				}
+				case TargetType.AllDeadOrUnconscious: {
+					if (component.Unconscious || component.Dead) {
 						return true;
 					}
 					break;
-				case TargetType.HostileWithGrimoire:
-					if (component3 != null && component3.IsHostile(target)
-						|| targetFaction != null && targetFaction.IsHostile(caster)) {
-						var component5 = target.GetComponent<Equipment>();
-						if (component5 != null && component5.CurrentItems != null
-							&& component5.CurrentItems.Grimoire != null) {
+				}
+				case TargetType.HostileWithGrimoire: {
+					if (HostileEvenIfConfused(target, caster)) {
+						Equipment equipment = target.GetComponent<Equipment>();
+						if (equipment != null && equipment.CurrentItems != null && equipment.CurrentItems.Grimoire != null) {
 							return true;
 						}
 					}
 					break;
-				case TargetType.HostileVessel:
-					if ((component3 != null && component3.IsHostile(target)
-						|| targetFaction != null && targetFaction.IsHostile(caster))
-						&& (component2 != null && component2.CharacterRace == CharacterStats.Race.Vessel)) {
+				}
+				case TargetType.HostileVessel: {
+					if (HostileEvenIfConfused(target, caster)
+						&& characterStat != null && characterStat.CharacterRace == CharacterStats.Race.Vessel) {
 						return true;
 					}
 					break;
-				case TargetType.HostileBeast:
-					if ((component3 != null && component3.IsHostile(target)
-						|| targetFaction != null && targetFaction.IsHostile(caster))
-						&& (component2 != null && component2.CharacterRace == CharacterStats.Race.Beast)) {
+				}
+				case TargetType.HostileBeast: {
+					if (HostileEvenIfConfused(target, caster)
+						&& characterStat != null && characterStat.CharacterRace == CharacterStats.Race.Beast) {
 						return true;
 					}
 					break;
-				case TargetType.Dead:
-					if (component1.Dead) {
+				}
+				case TargetType.Dead: {
+					if (component.Dead) {
 						return true;
 					}
 					break;
-				case TargetType.Ally:
-					var component6 = caster.GetComponent<PartyMemberAI>();
-					var component7 = target.GetComponent<PartyMemberAI>();
-					if (component6 != null && component6.gameObject.activeInHierarchy) {
-						if (component7 != null && component7.gameObject.activeInHierarchy) {
+				}
+				case TargetType.Ally: {
+					PartyMemberAI partyMemberAI = caster.GetComponent<PartyMemberAI>();
+					PartyMemberAI partyMemberAI1 = target.GetComponent<PartyMemberAI>();
+					if (partyMemberAI != null && partyMemberAI.gameObject.activeInHierarchy) {
+						if (partyMemberAI1 != null && partyMemberAI1.gameObject.activeInHierarchy) {
 							return true;
 						}
-						break;
-					}
-					if (component7 == null || !component7.gameObject.activeInHierarchy) {
+					} else if (partyMemberAI1 == null || !partyMemberAI1.gameObject.activeInHierarchy) {
 						return true;
 					}
 					break;
-				case TargetType.AllyNotSelf:
+				}
+				case TargetType.AllyNotSelf: {
 					if (target != caster && IsValidTarget(target, caster, TargetType.Ally)) {
 						return true;
 					}
 					break;
-				case TargetType.AllyNotSelfOrHostile:
-					if (IsValidTarget(target, caster, TargetType.Hostile) || IsValidTarget(target, caster, TargetType.AllyNotSelf)) {
+				}
+				case TargetType.AllyNotSelfOrHostile: {
+					if (IsValidTarget(target, caster, TargetType.Hostile)
+						|| IsValidTarget(target, caster, TargetType.AllyNotSelf)) {
 						return true;
 					}
 					break;
-				case TargetType.NotSelf:
-					if (target != caster && !component1.Unconscious && !component1.Dead) {
+				}
+				case TargetType.NotSelf: {
+					if (target != caster) {
 						return true;
 					}
 					break;
-				case TargetType.DragonOrDrake:
-					if (component2 != null
-						&& (component2.CharacterClass == CharacterStats.Class.Drake
-							|| component2.CharacterClass == CharacterStats.Class.AdraDragon
-							|| component2.CharacterClass == CharacterStats.Class.SkyDragon)) {
+				}
+				case TargetType.DragonOrDrake: {
+					if (characterStat != null
+						&& (characterStat.CharacterClass == CharacterStats.Class.Drake
+							|| characterStat.CharacterClass == CharacterStats.Class.AdraDragon
+							|| characterStat.CharacterClass == CharacterStats.Class.SkyDragon)) {
 						return true;
 					}
 					break;
-				case TargetType.FriendlyIncludingCharmed:
-					if (!component1.Unconscious && !component1.Dead) {
-						if (component3 != null && !component3.IsHostile(target)) {
-							return true;
-						}
-						if (caster != null) {
-							var component5 = caster.GetComponent<Faction>();
-							if (component5 != null && activeAiController != null) {
-								var originalTeam = activeAiController.GetOriginalTeam();
-								if (originalTeam != null
-									&& originalTeam.GetRelationship(component5.CurrentTeam) != Faction.Relationship.Hostile) {
-									return true;
-								}
+				} case TargetType.FriendlyIncludingCharmed: {
+					if (!HostileEvenIfConfused(target, caster)) {
+						return true;
+					}
+					if (caster != null) {
+						Faction faction1 = caster.GetComponent<Faction>();
+						if (faction1 != null && aIController != null) {
+							Team originalTeam = aIController.GetOriginalTeam();
+							if (originalTeam != null && originalTeam.GetRelationship(faction1.CurrentTeam) != Faction.Relationship.Hostile) {
+								return true;
 							}
 						}
 					}
 					break;
-				case TargetType.Self:
-					if (target == caster && !component1.Unconscious && !component1.Dead) {
+				}
+				case TargetType.Self: {
+					if (target == caster) {
 						return true;
 					}
 					break;
+				}
+				case TargetType.FriendlyNotVessel: {
+					if (characterStat != null && characterStat.CharacterRace != CharacterStats.Race.Vessel && !HostileEvenIfConfused(target, caster)) {
+						return true;
+					}
+					break;
+				}
 			}
+			//!+END MODIFICATIONS
 			return false;
 		}
 	}
