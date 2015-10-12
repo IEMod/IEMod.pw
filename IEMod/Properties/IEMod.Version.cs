9940aaed2d9f2f644fec5e4fd290abb630ac9ac9
tjayharvey <tjayharvey@gmail.com>
2015-10-11 21:23:52 -0700

    Updated versio number to reflect beta state

--- IEMod/Properties/IEMod.Version.cs
+++ IEMod/Properties/IEMod.Version.cs
@@ -7,6 +7,6 @@ namespace IEMod {
 	[NewType]
 	public static class IEModVersion {
 		public const string StupidVersion = "5.0.6";
-		public const string Version = "5.0.6";
+		public const string Version = "5.0.6beta";
 	}
 }
