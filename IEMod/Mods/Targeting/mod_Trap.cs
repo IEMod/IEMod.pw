18c470ce4d16beda909be748ce1f8055518ebe99
SiliconMage <dansub0@gmail.com>
2016-02-28 23:21:11 -0500

    Updated Code
    Updated the Disable Friendly Fire mod and added the Hide Anticlass
    Spells from Ability Bar mod.

--- IEMod/Mods/Targeting/mod_Trap.cs
+++ IEMod/Mods/Targeting/mod_Trap.cs
@@ -10,8 +10,9 @@ namespace IEMod.Mods.Targeting {
 		[NewMember]
 		[DuplicatesBody(nameof(CanActivate))]
 		private bool orig_CanActivate(GameObject victim) {
-			return false;
-		}
+            
+            return false;
+        }
 
 		[ModifiesMember(nameof(CanActivate))]
 		public bool mod_CanActivate(GameObject victim) {
@@ -28,51 +29,6 @@ namespace IEMod.Mods.Targeting {
 				}
 			}
 			return orig_CanActivate(victim);
-		}
-	
-
-		//[ModifiesMember("CanActivate")]
-		private bool CanActivateNew(GameObject victim) {
-			bool disableFriendlyFire = IEModOptions.DisableFriendlyFire;
-
-			if (!this.m_trap_initialized)
-			{
-				Debug.LogError("Cannot activate uninitialized trap!");
-				return false;
-			}
-
-			if (disableFriendlyFire)
-			{
-				Faction victimFaction = victim.GetComponent<Faction>();
-
-				if (victimFaction.isPartyMember && this.IsPlayerOwnedTrap)
-				{
-					return false;
-				}
-			}
-
-			if (this.Disarmed || this.m_triggerVictim != null)
-			{
-				return false;
-			}
-			if (this.MaxHitCount > 0 && this.m_numHits >= this.MaxHitCount)
-			{
-				return false;
-			}
-			if (GameUtilities.IsAnimalCompanion(victim))
-			{
-				return false;
-			}
-			if (this.m_ocl != null)
-			{
-				return true;
-			}
-			if (this.ActivatesForAnyone)
-			{
-				return true;
-			}
-			Faction component = victim.GetComponent<Faction>();
-			return component != null && (component.IsHostile(base.gameObject) || this.m_trap_faction.IsHostile(victim));
 		}
 	}
 }
